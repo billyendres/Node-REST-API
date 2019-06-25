fc4c8bec2e2c0e4cf7ffc2145320c4bbda84e0e1
BILLY ENDRES <billyendres@BILLYs-MacBook-Pro.local>
2019-06-25 17:41:25 +1000

    build added client

--- index.js
+++ index.js
@@ -9,11 +9,21 @@ mongoose.connect(keys.mongoURI);
 
 const app = express();
 
+//Only runs this code if in production - Heroku Deploy
+if (process.env.NODE_ENV === "production") {
+	// Express will serve up production assets ( main.js, main.css etc)
+	app.use(express.static("client/build"));
+	//Express will serve up Index.html if it doesn't recognise the route
+	const path = require("path");
+	app.get("*", (req, res) => {
+		res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+	});
+}
+
 //Setup Dynamic PORT
 const PORT = process.env.PORT || 5500;
 
 //Heroku Open to start
 //Install nodemon add to package.json
 //npm run dev to start server
-//navigate to PORT/auth/google to login
 app.listen(PORT, () => console.log(`server on port ${PORT}`));
